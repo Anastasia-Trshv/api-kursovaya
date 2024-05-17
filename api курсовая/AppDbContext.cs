099c64c16606bcd148c5e1351a8b1e1b824ebcb3
Анастасия Трушева <a.a.trusheva@stud.mospolytech.ru>
2024-05-17 18:45:48 +0300

    создана бд

---
+++ api курсовая/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using api_курсовая.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_курсовая
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Supply> Supplies { get; set; }
+        public DbSet<SupplyType> SupplyTypes { get; set; }
+        public DbSet<ShoppingCart> ShoppingCart { get; set; }
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-OHI96F6C\\SQLEXPRESS;Database=Kursovaya4sem;" +
+                "Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShoppingCart>().HasKey(u => new { u.UserId, u.SupplyId });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- api курсовая/model/ShoppingCart.cs
+++ api курсовая/model/ShoppingCart.cs
@@ -3,7 +3,8 @@
     public class ShoppingCart
     {
         public int UserId { get; set; }
-        public int SupplyId { get;}
+        public int SupplyId { get; set; }
+        public int Amount { get; set; }
 
     }
 }
